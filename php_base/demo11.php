584b1e707567aa53f37a3b6ec45b00ea0642dc14
MingJun <rioamo66@gmail.com>
2015-09-17 15:16:53 +0800

    add preg

--- php_base/demo11.php
+++ php_base/demo11.php
@@ -12,5 +12,41 @@ class demo11 {
     }
 }
 
-$d = new demo11();
-$d->run();
+//$d = new demo11();
+//$d->run();
+
+
+/*
+ * 正则表达式
+ * preg_
+ *
+ *
+ *
+ */
+$con = preg_quote(file_get_contents('http://www.baidu.com/'));
+$href = "<a rel='ddd.s' href='addd.g'>";
+//print_r($con);
+//$con = trim($con);
+$img = "/\<img\s*src=\".*?\"\/\>/iU";
+$mag = "/\<link\s*rel=\".*?\"\s*href=\"*?\"\/\>/iU";
+$script = "/\<script>(.*)<\/script>/iU";
+$title = "/<title>(.*)<\/title>/Ui";
+$ig = preg_match_all($title,$con);
+//
+//echo "<br>";
+//print_r($ig);
+
+$url = "http://www.baidu.com";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+$data = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+$mar = preg_match_all($script, $data);
+print_r($mar);
+//print_r($data);
