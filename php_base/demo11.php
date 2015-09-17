c1b05532aa293b69b1792c61bbc75e58ce426b41
MingJun <rioamo66@gmail.com>
2015-09-17 10:23:17 +0800

    react

---
+++ php_base/demo11.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mingjun
+ * Date: 2015/9/17
+ * Time: 9:48
+ */
+
+class demo11 {
+    function run(){
+        echo substr("adbcdwrerer",2,2);
+    }
+}
+
+$d = new demo11();
+$d->run();
