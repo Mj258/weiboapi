43b8afe15dfdcc2fff2ea158dc876a2183c92aac
MingJun <rioamo66@gmail.com>
2015-09-26 21:14:06 +0800

    angular

--- anjularjs/js/controller.js
+++ anjularjs/js/controller.js
@@ -1,3 +1,10 @@
-/**
- * Created by Mingjun on 2015/9/26.
- */
+function PhoneListCtrl($scope) {
+    $scope.phones = [
+        {"name": "Nexus S",
+            "snippet": "Fast just got faster with Nexus S."},
+        {"name": "Motorola XOOM™ with Wi-Fi",
+            "snippet": "The Next, Next Generation tablet."},
+        {"name": "MOTOROLA XOOM™",
+            "snippet": "The Next, Next Generation tablet."}
+    ];
+}
