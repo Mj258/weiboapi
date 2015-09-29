6dd97ed9732b3094f7d26dfc3692c8eed0b2eaf6
MingJun <rioamo66@gmail.com>
2015-09-30 00:39:08 +0800

    gruntjs

---
+++ gruntjs/Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            //build: {
+            //    src: 'src/<%= pkg.name %>.js',
+            //    dest: 'build/<%= pkg.name %>.min.js'
+            //}
+        },
+        watch: {
+            scripts: {
+                files: ['src/**/*.js'],
+                tasks: ['minall'],
+                options: {
+                    spawn: true,
+                    interrupt: true
+                }
+            }
+        },
+        compass: {                  // Task
+            dist: {                   // Target
+                options: {              // Target options
+                    sassDir: 'sass',
+                    cssDir: 'css',
+                    environment: 'production'
+                }
+            },
+            dev: {                    // Another target
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'css'
+                }
+            }
+        }
+    });
+
+    // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['uglify','compass']);
+
+};
